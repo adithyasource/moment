6098aa65832b5480d86dccd7322299d6df0a081f
adithyasource <me@adithya.zip>
2023-10-17 11:50:27 +0530

    Switching from ExpressJS to SvelteKit

---
+++ website/src/routes/[username]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { page } from "$app/stores";
+
+  let username = $page.params.username;
+</script>
+
+<iframe
+  src="https://firebasestorage.googleapis.com/v0/b/moment-4b33f.appspot.com/o/{username}?alt=media"
+  width="100%"
+  height="100%"
+  frameBorder="0"
+  style="padding:0px; margin:0px;"
+/>
